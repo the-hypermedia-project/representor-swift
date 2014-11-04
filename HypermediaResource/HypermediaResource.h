75e07c0b9240248df3186cad7061d4802cef6966
Kyle Fuller <inbox@kylefuller.co.uk>
2014-11-04 14:01:20 +0000

    Initial Xcode project

---
+++ HypermediaResource/HypermediaResource.h
@@ -0,0 +1,19 @@
+//
+//  HypermediaResource.h
+//  HypermediaResource
+//
+//  Created by Kyle Fuller on 04/11/2014.
+//  Copyright (c) 2014 Apiary. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HypermediaResource.
+FOUNDATION_EXPORT double HypermediaResourceVersionNumber;
+
+//! Project version string for HypermediaResource.
+FOUNDATION_EXPORT const unsigned char HypermediaResourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HypermediaResource/PublicHeader.h>
+
+
