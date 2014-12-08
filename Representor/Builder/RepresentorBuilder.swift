02cf1239e06245065e27730014609a9aa0355c74
Kyle Fuller <inbox@kylefuller.co.uk>
2014-12-08 20:36:12 +0000

    [Builder] Document methods

--- Representor/Builder/RepresentorBuilder.swift
+++ Representor/Builder/RepresentorBuilder.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/// A class used to build a representor using a builder pattern
 public class RepresentorBuilder {
   var transitions = Dictionary<String, Transition>()
   var representors = Dictionary<String, [Representor]>()
@@ -15,12 +16,20 @@ public class RepresentorBuilder {
   var links = Dictionary<String, String>()
   var metadata = Dictionary<String, String>()
 
+  /// Adds an attribute
+  ///
+  /// :param: name The name of the attribute
+  /// :param: value The value of the attribute
   public func addAttribute(name:String, value:AnyObject) {
     attributes[name] = value
   }
 
   // MARK: Representors
 
+  /// Adds an embedded representor
+  ///
+  /// :param: name The name of the representor
+  /// :param: representor The representor
   public func addRepresentor(name:String, representor:Representor) {
     if var representorSet = representors[name] {
       representorSet.append(representor)
@@ -30,21 +39,38 @@ public class RepresentorBuilder {
     }
   }
 
+  /// Adds an embedded representor using the builder pattern
+  ///
+  /// :param: name The name of the representor
+  /// :param: builder A builder to build the representor
   public func addRepresentor(name:String, block:((builder:RepresentorBuilder) -> ())) {
     addRepresentor(name, representor:Representor(block))
   }
 
   // MARK: Transition
 
+  /// Adds a transition
+  ///
+  /// :param: name The name (or relation) for the transition
+  /// :param: transition The transition
   public func addTransition(name:String, _ transition:Transition) {
     transitions[name] = transition
   }
 
+  /// Adds a transition with a URI
+  ///
+  /// :param: name The name (or relation) for the transition
+  /// :param: uri The URI of the transition
   public func addTransition(name:String, uri:String) {
     let transition = Transition(uri: uri, attributes:[:], parameters:[:])
     transitions[name] = transition
   }
 
+  /// Adds a transition with a URI using a builder
+  ///
+  /// :param: name The name (or relation) for the transition
+  /// :param: uri The URI of the transition
+  /// :param: builder The builder used to create the transition
   public func addTransition(name:String, uri:String, builder:((TransitionBuilder) -> ())) {
     let transition = Transition(uri: uri, builder)
     transitions[name] = transition
@@ -52,12 +78,20 @@ public class RepresentorBuilder {
 
   // MARK: Links
 
+  /// Adds a link
+  ///
+  /// :param: name The name (or relation) for the link
+  /// :param: uri The URI of the link
   public func addLink(name:String, uri:String) {
     links[name] = uri
   }
 
   // MARK: Metadata
 
+  /// Adds an piece of metadata
+  ///
+  /// :param: key The key for the metadata
+  /// :param: value The value of the key
   public func addMetaData(key:String, value:String) {
     metadata[key] = value
   }
--- Representor/Builder/TransitionBuilder.swift
+++ Representor/Builder/TransitionBuilder.swift
@@ -8,17 +8,26 @@
 
 import Foundation
 
+/// A class used to build a transition using a builder pattern
 public class TransitionBuilder {
   var attributes = InputProperties()
   var parameters = InputProperties()
 
   // MARK: Attributes
 
+  /// Adds an attribute without a value or default value
+  ///
+  /// :param: name The name of the attribute
   public func addAttribute(name:String) {
     let property = InputProperty<AnyObject>(value:nil, defaultValue:nil)
     attributes[name] = property
   }
 
+  /// Adds an attribute with a value or default value
+  ///
+  /// :param: name The name of the attribute
+  /// :param: value The value of the attribute
+  /// :param: value The default value of the attribute
   public func addAttribute<T : AnyObject>(name:String, value:T?, defaultValue:T?) {
     let property = InputProperty<AnyObject>(value:value, defaultValue:defaultValue)
     attributes[name] = property
@@ -26,11 +35,19 @@ public class TransitionBuilder {
 
   // MARK: Parameters
 
+  /// Adds a parameter without a value or default value
+  ///
+  /// :param: name The name of the attribute
   public func addParameter(name:String) {
     let property = InputProperty<AnyObject>(value:nil, defaultValue:nil)
     parameters[name] = property
   }
 
+  /// Adds a parameter with a value or default value
+  ///
+  /// :param: name The name of the attribute
+  /// :param: value The value of the attribute
+  /// :param: value The default value of the attribute
   public func addParameter<T : AnyObject>(name:String, value:T?, defaultValue:T?) {
     let property = InputProperty<AnyObject>(value:value, defaultValue:defaultValue)
     parameters[name] = property
