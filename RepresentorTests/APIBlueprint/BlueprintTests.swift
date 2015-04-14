23287e06069a13c9da641bda45e4630c5b19c154
Kyle Fuller <kyle@fuller.li>
2015-04-14 15:49:34 -0700

    Use Polls API Blueprint as a test fixture

--- RepresentorTests/APIBlueprint/BlueprintTests.swift
+++ RepresentorTests/APIBlueprint/BlueprintTests.swift
@@ -176,72 +176,40 @@ class BlueprintTests : XCTestCase {
     let bundle = NSBundle(forClass:object_getClass(self))
     let blueprint = Blueprint(named:"blueprint.json", bundle:bundle)!
 
-    XCTAssertEqual(blueprint.name, "Requests API")
-    XCTAssertEqual(blueprint.description!, "Following the [Responses](5.%20Responses.md) example, this API will show you how to define multiple requests and what data these requests can bear. Let's demonstrate multiple requests on a trivial example of content negotiation.\n\n## API Blueprint\n\n+ [Previous: Responses](5.%20Responses.md)\n\n+ [This: Raw API Blueprint](https://raw.github.com/apiaryio/api-blueprint/master/examples/6.%20Requests.md)\n\n+ [Next: Parameters](7.%20Parameters.md)\n\n")
+    XCTAssertEqual(blueprint.name, "Polls")
+    XCTAssertEqual(blueprint.description!, "Polls is a simple web service that allows consumers to view polls and vote in them.\n\n")
 
     let resourceGroups = blueprint.resourceGroups
-    let resourceGroup = resourceGroups[0]
-    XCTAssertEqual(resourceGroups.count, 1)
-    XCTAssertEqual(resourceGroup.name, "Messages")
-    XCTAssertEqual(resourceGroup.description!, "Group of all messages-related resources.\n\n")
+    let resourceGroup = resourceGroups[1]
+    XCTAssertEqual(resourceGroups.count, 2)
+    XCTAssertEqual(resourceGroup.name, "Question")
+    XCTAssertEqual(resourceGroup.description!, "Resource related to questions in the API.\n\n")
 
     let resource = resourceGroup.resources[0]
-    XCTAssertEqual(resourceGroup.resources.count, 1)
-    XCTAssertEqual(resource.name, "My Message")
-    XCTAssertEqual(resource.description!, "")
-    XCTAssertEqual(resource.uriTemplate, "/message")
+    XCTAssertEqual(resourceGroup.resources.count, 3)
+    XCTAssertEqual(resource.name, "Question")
+    XCTAssertTrue(resource.description!.hasPrefix("A Question object has the following attributes."))
+    XCTAssertEqual(resource.uriTemplate, "/questions/{question_id}")
 
     let retrieveAction = resource.actions[0]
-    let updateAction = resource.actions[1]
-    XCTAssertEqual(resource.actions.count, 2)
+    XCTAssertEqual(resource.actions.count, 1)
 
-    XCTAssertEqual(retrieveAction.name, "Retrieve a Message")
-    XCTAssertEqual(retrieveAction.description!, "In API Blueprint requests can hold exactly the same kind of information and can be described by exactly the same structure as responses, only with different signature – using the `Request` keyword. The string that follows after the `Request` keyword is a request identifier. Again, using an explanatory and simple naming is the best way to go. \n\n")
+    XCTAssertEqual(retrieveAction.name, "View a question detail")
     XCTAssertEqual(retrieveAction.method, "GET")
 
-    XCTAssertEqual(retrieveAction.examples.count, 2)
+    XCTAssertEqual(retrieveAction.examples.count, 1)
 
     let example1 = retrieveAction.examples[0]
     XCTAssertEqual(example1.name, "")
     XCTAssertEqual(example1.description!, "")
-    XCTAssertEqual(example1.requests.count, 1)
+    XCTAssertEqual(example1.requests.count, 0)
     XCTAssertEqual(example1.responses.count, 1)
-
-    let requestPayload = example1.requests[0]
-    XCTAssertEqual(requestPayload.name, "Plain Text Message")
-    XCTAssertEqual(requestPayload.description!, "")
-    XCTAssertEqual(requestPayload.headers.count, 1)
-    XCTAssertEqual(requestPayload.headers[0].name, "Accept")
-    XCTAssertEqual(requestPayload.headers[0].value, "text/plain")
 
     let responsePayload = example1.responses[0]
     XCTAssertEqual(responsePayload.name, "200")
     XCTAssertEqual(responsePayload.description!, "")
-    XCTAssertEqual(responsePayload.headers.count, 2)
+    XCTAssertEqual(responsePayload.headers.count, 1)
     XCTAssertEqual(responsePayload.headers[0].name, "Content-Type")
-    XCTAssertEqual(responsePayload.headers[0].value, "text/plain")
-    XCTAssertEqual(responsePayload.headers[1].name, "X-My-Message-Header")
-    XCTAssertEqual(responsePayload.headers[1].value, "42")
-    let body = "Hello World!\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)!
-    XCTAssertEqual(responsePayload.body!, body)
-
-    let example2 = retrieveAction.examples[1]
-    XCTAssertEqual(example2.name, "")
-    XCTAssertEqual(example2.description!, "")
-    XCTAssertEqual(example2.requests.count, 1)
-    XCTAssertEqual(example2.responses.count, 1)
-
-    let parameter = retrieveAction.parameters[0]
-    XCTAssertEqual(parameter.name, "id")
-    XCTAssertEqual(parameter.description!, "An unique identifier of the message.")
-    XCTAssertEqual(parameter.type!, "number")
-    XCTAssertTrue(parameter.required)
-    XCTAssertEqual(parameter.defaultValue!, "")
-    XCTAssertEqual(parameter.example!, "1")
-    XCTAssertEqual(parameter.values!, [])
-
-    XCTAssertEqual(updateAction.name, "Update a Message")
-    XCTAssertEqual(updateAction.description!, "")
-    XCTAssertEqual(updateAction.method, "PUT")
+    XCTAssertEqual(responsePayload.headers[0].value, "application/json")
   }
 }
