88f83662e8a941bc92f9a8fea8f4ec80c0fe02cd
Z <z@apiary.io>
2015-08-08 15:07:26 +0200

    GitHub media type work around
    Unfortunately GitHub does not respect their own
    `application/vnd.github.v3+json` media type in Content-Type.
    
    A  call with `Accept: application/vnd.github.v3+json` returns:
    
    -  `Conent-Type: application/json; charset=utf-8`
    - `X-GitHub-Media-Type: github.v3; format=json`
    
    This commit adds a work around until GitHub gets content negotiation
    right.

--- Representor/HTTP/HTTPDeserialization.swift
+++ Representor/HTTP/HTTPDeserialization.swift
@@ -32,6 +32,11 @@ public struct HTTPDeserialization {
     "application/vnd.siren+json": jsonDeserializer { payload in
       return deserializeSiren(payload)
     },
+    
+    "application/vnd.github.v3+json": jsonDeserializer { payload in
+      return deserializeGitHub(payload)
+    },
+    
   ]
 
   /// An array of the supported content types in order of preference
@@ -51,7 +56,16 @@ public struct HTTPDeserialization {
       if let deserializer = HTTPDeserialization.deserializers[contentType] {
         return deserializer(response: response, body: body)
       }
+      else if let githubMediaType = response.allHeaderFields["X-GitHub-Media-Type"] as? String {
+        // Work around for GitHub media type
+        if githubMediaType == "github.v3; format=json" {
+          if let deserializer = HTTPDeserialization.deserializers["application/vnd.github.v3+json"] {
+            return deserializer(response: response, body: body)
+          }
+        }
+      }
     }
+    
 
     return nil
   }
