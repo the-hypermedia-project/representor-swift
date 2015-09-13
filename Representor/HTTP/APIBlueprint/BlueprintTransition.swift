ca4b68d29c73837098f87ca98e60ba1f4bc6a493
Kyle Fuller <kyle@fuller.li>
2015-09-13 13:32:10 -0700

    Swift 2.0 array has it's own contains

--- Representor/HTTP/APIBlueprint/BlueprintTransition.swift
+++ Representor/HTTP/APIBlueprint/BlueprintTransition.swift
@@ -8,13 +8,6 @@
 
 import Foundation
 
-extension Array {
-  func contains<T : Equatable>(obj: T) -> Bool {
-    let filtered = self.filter { $0 as? T == obj }
-    return !filtered.isEmpty
-  }
-}
-
 extension Resource {
   func transition(actionName:String) -> HTTPTransition? {
     func filterAction(action:Action) -> Bool {
